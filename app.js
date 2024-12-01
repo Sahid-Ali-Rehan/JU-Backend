e36f00f344c8fd2ec903d3ed577fb10a6fa70de3
Sahid Ali <sahidalirehan@gmail.com>
2024-12-01 17:35:31 +0600

    third commit

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
+// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 const cors = require("cors");
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-require('dotenv').config();
 
 dotenv.config();
 connectDB();
 
-const authRoutes = require('./routes/auth');
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./api/auth');
+const productRoutes = require('./api/products');
+const userRoutes = require('./api/users');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-
-
-
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Auth API');
 });
 
-
-
+// Export the express app as a serverless function for Vercel
 module.exports = app;
